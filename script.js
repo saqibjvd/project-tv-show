93740341a68d8f0aebe0b9f5950e477ee3b1c6fe
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-12-12 20:16:52 +0000

    Level 100, rendering all episodes

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+   const {name, summary, image, number, season} = episode
+   const code = `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`
+   const markUp = `
+   <div class="episode">
+      <div class="episode-header">
+      <h3>${name}</h3>
+      <p>${code}</p>
+      </div>
+      <img src="${image.medium}" alt="${name}">
+      <div class="summary">
+        ${summary}
+      </div>
+    </div>
+   `
+   rootElem.insertAdjacentHTML("beforeend", markUp)
+  })
 }
+
 
 window.onload = setup;
